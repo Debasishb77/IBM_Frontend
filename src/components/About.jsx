ab6eaa5951b6f6c2f31597272b6281a3c655d090
biswa <biswabaibhab007@gmail.com>
2024-08-02 07:18:37 +0530

    responsiveness fixes for max-width: 1600px

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,61 +9,6 @@ import { FaPinterestP } from "react-icons/fa";
 function About() {
 
   return (
-    // <div className="parent-con">
-    //   <section className="section-white">
-    //     <div className="container">
-    //       <div className="row">
-    //         <div className="col-md-12 text-center">
-    //           <h2 className="pt-4 section-title">The Team of <span className="text-[#023460]">WEB WIZARDS</span></h2>
-    //           <p className="section-subtitle"></p>
-    //         </div>
-    //         <div className="page-content flex justify-center">
-    //           <div className="img-contt">
-    //             <img src="https://img.freepik.com/free-vector/meeting-concept-illustration_114360-717.jpg?t=st=1722515373~exp=1722518973~hmac=5617c1f0e940ab1792fdf62f797d8945e38d7d8d8b774850cfac14826dccb56e&w=740" className="imgg" alt="" />
-    //           </div>
-
-    //           <div className="team-text">
-    //             <div className="team-item ">
-    //               <div className="team-info">
-    //                 <h2>Who are we?</h2>
-    //               </div>
-    //               <div className="hnn-note">
-    //                 <p>
-    //                   Welcome to the Health and Nutrition Network (HNN), where we are dedicated to eradicating hunger and enhancing health in our communities. Our mission is to empower local communities by providing access to nutritious food, essential health services, and wellness education. We collaborate with NGOs and local organizations to ensure every community member thrives. Our platform includes donation portals, resource mapping, virtual workshops, telemedicine services, and community forums. Join us in creating sustainable solutions for hunger and health, driving positive social impact, and building healthier, hunger-free communities. Together, we can make a difference.
-    //                 </p>
-    //               </div>
-    //               <ul className="team-icon ">
-    //                 <li>
-    //                   <a href="#" className="twitter ">
-    //                     <FaTwitter className="" />
-    //                   </a>
-    //                 </li>
-    //                 <li>
-    //                   <a href="#" className="pinterest">
-    //                     <FaPinterestP />
-    //                   </a>
-    //                 </li>
-    //                 <li>
-    //                   <a href="#" className="facebook">
-    //                     <FaFacebookF />
-    //                   </a>
-    //                 </li>
-    //                 <li>
-    //                   <a href="#" className="dribble">
-    //                     <FaInstagram />
-    //                   </a>
-    //                 </li>
-    //               </ul>
-    //             </div>
-    //           </div>
-    //         </div>
-
-    //       </div>
-    //     </div>
-    //   </section>
-
-    // </div>
-
     <div className="about-parent">
       <div className="about-heading-div">
         <h1 className="about-heading">The team behind HNN - <span>WEB WIZARDS</span></h1>
@@ -75,9 +20,9 @@ function About() {
         </div>
         {/* text-section */}
         <div className="about-text-container">
-          <h3 className="about-text-heading">Idea Behind HNN</h3> <hr />
+          <h3 className="about-text-heading">What HNN is about ?</h3> <hr />
           <p className="about-text">
-            Welcome to the Health and Nutrition Network (HNN), where we are dedicated to eradicating hunger and enhancing health in our communities. Our mission is to empower local communities by providing access to nutritious food, essential health services, and wellness education. We collaborate with NGOs and local organizations to ensure every community member thrives. Our platform includes donation portals, resource mapping, virtual workshops, telemedicine services, and community forums. Join us in creating sustainable solutions for hunger and health, driving positive social impact, and building healthier, hunger-free communities. Together, we can make a difference.
+            The Health and Nutrition Network (HNN), aims to eradicate hunger and enhance health in our communities. Our mission is to empower local communities by providing access to nutritious food, essential health services, and wellness education. We collaborate with NGOs and local organizations to ensure every community member thrives. Our platform includes donation portals, resource mapping, virtual workshops, telemedicine services, and community forums. Join us in creating sustainable solutions for hunger and health, driving positive social impact, and building healthier, hunger-free communities. <span>Together, we can make a difference.</span>
           </p>
         </div>
       </div>
